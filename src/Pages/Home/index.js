8e9104ea8ea87269daf0b398834d0fcae227592a
DavyHyun <dhyun1124@gmail.com>
2023-07-27 20:49:09 -0400

    swiper added

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,16 +1,59 @@
-const Home = () => {
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import image1 from '../../assets/crew.png'
+import image2 from '../../assets/jericho.png'
+import image3 from '../../assets/redwood.png'
+
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+
+import styles from './index.module.scss'
+
+function index() {
     return (
-        <div>
-            <h1>HOME</h1>
-            <h1>HOME</h1>
-            <h1>HOME</h1>
-            <h1>HOME</h1>
-            <h1>HOME</h1>
-            <h1>HOME</h1>
-            <h1>HOME</h1>
-            <h1>HOME</h1>
+        <div className={styles.wrapper}>
+          <Swiper
+            effect={'coverflow'}
+            grabCursor={true}
+            initialSlide={1}
+            centeredSlides={true}
+            // loop={true}
+            slidesPerView={'auto'}
+            coverflowEffect={{
+                rotate: 0,
+                stretch: -30,
+                depth: 40,
+                modifier: 20,
+                slideShadows: false,
+            }}
+            slideToClickedSlide={true}
+            speed={1000}
+            pagination={{ el: '.swiper-pagination', clickable: true }}
+            navigation={{
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+              clickable: true,
+            }}
+            modules={[EffectCoverflow, Pagination, Navigation]}
+            className={styles.swiper_container}
+          >
+            <SwiperSlide className={styles.swiper_slide}>
+              <img className={styles.img} src={image1} alt="slide_image" />
+            </SwiperSlide>
+            <SwiperSlide className={styles.swiper_slide}>
+              <img className={styles.imgComputer} src={image2} alt="slide_image" />
+            </SwiperSlide>
+            <SwiperSlide className={styles.swiper_slide}>
+              <img className={styles.img} src={image1} alt="slide_image" />
+            </SwiperSlide>
+          </Swiper>
         </div>
     )
 }
 
-export default Home;
+export default index
