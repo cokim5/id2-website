4ae4d1ab2bcb532e9ea0158006f9fb81f39fcec3
cokim5 <54604408+cokim5@users.noreply.github.com>
2023-08-21 18:54:02 -0700

    updated

--- src/Pages/News/index.js
+++ src/Pages/News/index.js
@@ -1,16 +1,96 @@
+// import LocomotiveScroll from 'locomotive-scroll';
+// import styles from './index.module.scss';
+// import React, { useEffect } from 'react';
+
+
+// const News = () => {
+
+
+//     return (
+//         <div>
+//             <div className={styles.welcomeContainer}>
+//                 <img
+//                     src={require('../../assets/crewLogo.png')}
+//                     className={styles.crewLogo}
+//                 />
+//                 <div className={styles.welcomeText}>
+//                     Welcome to CREW!
+//                 </div>
+//                 <div className={styles.screenContainer}>
+
+
+//                     <img
+//                         src={require('../../assets/crew.png')}
+//                         className={styles.crewWelcomeScreen}
+//                     />
+//                 </div>
+//             </div>
+
+//             <div className={styles.firstSlide}>
+//                 WJOIFJIOEWJ
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default News;
+
+
+
+
+
+
+import React, { useEffect, useRef } from 'react';
+import LocomotiveScroll from 'locomotive-scroll';
+import styles from './index.module.scss';
+
 const News = () => {
+    const scrollRef = useRef(null);
+
+    useEffect(() => {
+        const scroll = new LocomotiveScroll({
+            el: scrollRef.current,
+            smooth: true,
+            smoothMobile: false,
+        });
+
+        return () => {
+            if (scroll) {
+                scroll.destroy();
+            }
+        };
+    }, []);
+
     return (
-        <div>
-            <h1>NEWS</h1>
-            <h1>NEWS</h1>
-            <h1>NEWS</h1>
-            <h1>NEWS</h1>
-            <h1>NEWS</h1>
-            <h1>NEWS</h1>
-            <h1>NEWS</h1>
-            <h1>NEWS</h1>
+        <div ref={scrollRef}>
+            <div className={styles.welcomeContainer} >
+                <div data-scroll-section>
+
+
+                    <img
+                        src={require('../../assets/crewLogo.png')}
+                        className={styles.crewLogo}
+                    />
+                    <div className={styles.welcomeText}>
+                        Welcome to CREW!
+                    </div>
+                </div>
+
+                <div className={styles.screenContainer} data-scroll-section>
+                    <img
+                        src={require('../../assets/crew.png')}
+                        className={styles.crewWelcomeScreen}
+                        data-scroll
+                    />
+                </div>
+            </div>
+            <div className={styles.firstSlide} data-scroll-section>
+                WJOIFJIOEWJ
+            </div>
         </div>
-    )
+    );
 }
 
 export default News;
+
+
