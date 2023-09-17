767a0ad9179ba9f489128d7c8a1228a20e9f633f
cokim5 <54604408+cokim5@users.noreply.github.com>
2023-09-16 17:42:45 -0700

    added github to contact

--- src/Pages/Contact/index.js
+++ src/Pages/Contact/index.js
@@ -6,6 +6,7 @@ const Contact = () => {
     <div className={styles.container}>
       <p>email: contact@id2.company</p>
       <a href="https://www.linkedin.com/company/id2startup/" target="_blank" ><img src={require('../../assets/linkedin.png')} className={styles.image}/></a>
+      <a href="https://github.com/iD2-tech" target="_blank" ><img src={require('../../assets/github.png')} className={styles.image}/></a>
     </div>
   )
 }
